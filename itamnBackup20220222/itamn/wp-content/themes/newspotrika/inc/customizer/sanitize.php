37390b5bfc48b1590b7d1ef48871e7b5f9d3673d
bazaradna <55179797+bazaradna@users.noreply.github.com>
2022-03-13 17:50:19 +0800

    Revert "itasite"
    This reverts commit efb6baca50bf25e258688965de1a7b6ca39be02e.

--- itamnBackup20220222/itamn/wp-content/themes/newspotrika/inc/customizer/sanitize.php
+++
@@ -1,6 +0,0 @@
-<?php 
-//checkbox sanitization function
-function newspotrika_sanitize_checkbox( $input ){
-    //returns true if checkbox is checked
-    return ( ( isset( $input ) && true == $input ) ? true : false );
-}
